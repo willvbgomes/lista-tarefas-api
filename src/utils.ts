c6c1d219247646eba9fc06a35db264bba7ffe51a
William Vilas Boas Gomes <willzgomes@gmail.com>
2024-12-17 22:24:21 -0300

    update utils.ts file

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,31 @@
+type ErrorWithStatus = Error & {
+  status?: number
+}
+
+export const setNewError = (message: string, status: number = 500) => {
+  const error = new Error(message) as ErrorWithStatus
+  error.status = status
+
+  return error
+}
+
 export const handleError = (error: unknown) => {
-  if (error instanceof Error) {
-    return error.message
+  if (error instanceof Error && 'status' in error) {
+    return {
+      errorMessage: error.message,
+      errorStatus: error.status as number,
+    }
   }
 
-  return 'An unexpected error occurred.'
+  if (error instanceof Error) {
+    return {
+      errorMessage: error.message,
+      errorStatus: 500,
+    }
+  }
+
+  return {
+    errorMessage: 'An unexpected error occurred.',
+    errorStatus: 500,
+  }
 }
