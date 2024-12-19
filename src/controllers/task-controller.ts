c1506f3766753d248eacea33902a2ffb5b9f3349
William Vilas Boas Gomes <willzgomes@gmail.com>
2024-12-18 22:28:07 -0300

    update task-controller.ts file

--- src/controllers/task-controller.ts
+++ src/controllers/task-controller.ts
@@ -3,101 +3,64 @@ import { getListById } from '../models/list-model'
 import * as taskModel from '../models/task-model'
 import { handleError } from '../utils'
 
-export const getTasks = (req: Request, res: Response) => {
+export const getTasks = async (req: Request, res: Response) => {
   const { listId } = req.params
 
   try {
-    const list = getListById(listId)
+    const list = await getListById(listId)
 
     if (!list) {
       res.status(404).json({ message: 'List not found.' })
       return
     }
 
-    const tasks = taskModel.getAllTasks(list)
+    res.status(200).json(list.tasks)
+  } catch (error: unknown) {
+    const { errorMessage, errorStatus } = handleError(error)
 
-    res.status(200).json(tasks)
-  } catch (error) {
-    const errorMessage = handleError(error)
-
-    res.status(500).json({ message: errorMessage })
+    res.status(errorStatus).json({ message: errorMessage })
   }
 }
 
-export const createTask = (req: Request, res: Response) => {
+export const createTask = async (req: Request, res: Response) => {
   const { listId } = req.params
-  const taskBody = req.body
+  const { description } = req.body
 
   try {
-    const list = getListById(listId)
-
-    if (!list) {
-      res.status(404).json({ message: 'List not found.' })
-      return
-    }
-
-    const task = taskModel.createNewTask(list, taskBody)
+    const task = await taskModel.createNewTask(listId, description)
 
     res.status(201).json(task)
-  } catch (error) {
-    const errorMessage = handleError(error)
+  } catch (error: unknown) {
+    const { errorMessage, errorStatus } = handleError(error)
 
-    res.status(500).json({ message: errorMessage })
+    res.status(errorStatus).json({ message: errorMessage })
   }
 }
 
-export const updateTask = (req: Request, res: Response) => {
+export const updateTask = async (req: Request, res: Response) => {
   const { listId, taskId } = req.params
 
   try {
-    const list = getListById(listId)
-
-    if (!list) {
-      res.status(404).json({ message: 'List not found.' })
-      return
-    }
-
-    const task = taskModel.getTaskById(list, taskId)
-
-    if (!task) {
-      res.status(404).json({ message: 'Task not found.' })
-      return
-    }
-
-    taskModel.toggleTask(task)
+    const task = await taskModel.toggleTask(listId, taskId)
 
     res.status(200).json(task)
-  } catch (error) {
-    const errorMessage = handleError(error)
+  } catch (error: unknown) {
+    const { errorMessage, errorStatus } = handleError(error)
 
-    res.status(500).json({ message: errorMessage })
+    res.status(errorStatus).json({ message: errorMessage })
   }
 }
 
-export const deleteTask = (req: Request, res: Response) => {
+export const deleteTask = async (req: Request, res: Response) => {
   const { listId, taskId } = req.params
 
   try {
-    const list = getListById(listId)
-
-    if (!list) {
-      res.status(404).json({ message: 'List not found.' })
-      return
-    }
-
-    const task = taskModel.getTaskById(list, taskId)
-
-    if (!task) {
-      res.status(404).json({ message: 'Task not found.' })
-      return
-    }
-
-    taskModel.deleteTask(list, task)
+    await taskModel.deleteTask(listId, taskId)
 
     res.status(200).json({ message: 'The task has been successfully deleted.' })
-  } catch (error) {
-    const errorMessage = handleError(error)
+  } catch (error: unknown) {
+    const { errorMessage, errorStatus } = handleError(error)
 
-    res.status(500).json({ message: errorMessage })
+    res.status(errorStatus).json({ message: errorMessage })
   }
 }
